7277b09801056a9f8c4b1c81014ec29365cb7fe1
YepJared <97353828+YepJared@users.noreply.github.com>
2025-02-24 15:05:53 -0500

    Add code to create a new game and play a round
    Add code that creates a board of empty strings of a given size to play with
    Add a function to create a game object that only allows the reading of the turn and board and the ability to play into a specific square

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+function createBoard(size) {
+    const board = [];
+    for (let i = 0; i < size; i++) {
+        board.push([]);
+        for (let j = 0; j < size; j++) {
+            board[i].push('');
+        }
+    }
+    return board;
+}
+
+function newGame(size = 3) {
+    const board = createBoard(size);
+    let turn = 'X';
+
+    const play = (row, column) => {
+        board[row][column] = turn;
+        turn = turn === 'X' ? 'O' : 'X'
+    }
+
+    const getTurn = () => turn;
+    const getBoard = () => board;
+    return {play, getTurn, getBoard};
+}
