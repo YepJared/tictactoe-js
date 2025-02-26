d619063ad88c5d94925401a2d960e4136dfd5fa1
YepJared <97353828+YepJared@users.noreply.github.com>
2025-02-26 15:55:34 -0500

    Add check to ensure turns are only played in bounds

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+function inputOutOfBounds(r, c, size) {
+    const rowOutOfBounds = (r < 0 || r >= size);
+    const colOutOfBounds = (c < 0 || c >= size);
+    return (rowOutOfBounds || colOutOfBounds);
+}
+
 function isOccupiedSpace(space) {
     return (space === 'X' || space === 'O');
 }
@@ -14,6 +20,7 @@ function newGame(boardSize = 3) {
         }
 
         const getBoard = () => board;
+        const getSize = () => board.length;
         const getSpace = (row, column) => board[row][column];
         const updateSpace = (row, column, value) => board[row][column] = value;
         const reset = () => {
@@ -23,12 +30,17 @@ function newGame(boardSize = 3) {
                 }
             }
         }
-        return {getBoard, getSpace, updateSpace, reset};
+        return {getBoard, getSize, getSpace, updateSpace, reset};
     })();
 
     let turn = 'X';
 
     const play = (row, column) => {
+        if (inputOutOfBounds(row, column, gameBoard.getSize())) {
+            console.log("Invalid input. Try again.");
+            return;
+        }
+        
         if (isOccupiedSpace(gameBoard.getSpace(row, column))) {
             console.log("Invalid placement. Try again.");
             return;
